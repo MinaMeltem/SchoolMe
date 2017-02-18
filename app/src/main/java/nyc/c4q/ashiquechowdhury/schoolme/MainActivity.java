9576add8ff82dd74a57ffc70f2de3bfc11807b42
achowdhury3762 <achowdhury3762@gmail.com>
2017-02-18 14:28:22 -0500

    Bottom Nav

--- app/src/main/java/nyc/c4q/ashiquechowdhury/schoolme/MainActivity.java
+++ app/src/main/java/nyc/c4q/ashiquechowdhury/schoolme/MainActivity.java
@@ -1,13 +1,56 @@
 package nyc.c4q.ashiquechowdhury.schoolme;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupBottomNavigation();
+    }
+
+    private void setupBottomNavigation() {
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.profile_menu_item:
+                                inflateProfileFragment();
+                                break;
+                            case R.id.home_menu_item:
+                                inflateHomeFragment();
+                                break;
+                            case R.id.favorites_menu_item:
+                                inflateFavoritesFragment();
+                                break;
+                        }
+                        return false;
+                    }
+                }
+
+        );
+    }
+
+    private void inflateProfileFragment() {
+        getSupportFragmentManager().beginTransaction().add(R.id.activity_main, new ProfileFragment()).commit();
+    }
+
+    private void inflateHomeFragment() {
+        getSupportFragmentManager().beginTransaction().add(R.id.activity_main, new HomeFragment()).commit();
+    }
+
+
+    private void inflateFavoritesFragment() {
+        getSupportFragmentManager().beginTransaction().add(R.id.activity_main, new FavoritesFragment()).commit();
     }
 }
---
+++ app/src/main/java/nyc/c4q/ashiquechowdhury/schoolme/ProfileFragment.java
@@ -0,0 +1,23 @@
+package nyc.c4q.ashiquechowdhury.schoolme;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by ashiquechowdhury on 2/18/17.
+ */
+public class ProfileFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.fragment_profile, parent, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState){
+
+    }
+}
