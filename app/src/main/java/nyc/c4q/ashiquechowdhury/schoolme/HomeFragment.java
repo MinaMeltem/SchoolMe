41457fbbe483a764b282196bcc97c526c31ab677
jello <jdlopez114@gmail.com>
2017-02-19 14:04:17 -0500

    update

--- app/src/main/java/nyc/c4q/ashiquechowdhury/schoolme/HomeFragment.java
+++ app/src/main/java/nyc/c4q/ashiquechowdhury/schoolme/HomeFragment.java
@@ -39,7 +39,7 @@ public class HomeFragment extends Fragment implements SwipeStack.SwipeStackListe
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.swipe_view_fragment, container, false);
+        return inflater.inflate(R.layout.home_view_fragment, container, false);
     }
 
     @Override
