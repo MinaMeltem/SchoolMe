0f1667ec567820c5a3f04119348bafb85910ccc3
sderekj <dereksantos1989@gmail.com>
2017-02-18 21:27:48 -0500

    Fixed ActivityMain Layout Weight

--- app/src/main/java/nyc/c4q/ashiquechowdhury/schoolme/SchoolInfoFragment.java
+++ app/src/main/java/nyc/c4q/ashiquechowdhury/schoolme/SchoolInfoFragment.java
@@ -40,7 +40,7 @@ public class SchoolInfoFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.activityschoolinfo, container, false);
+        View view = inflater.inflate(R.layout.fragmentschoolinfo, container, false);
 
         appBarBackground = (ImageView) view.findViewById(R.id.appbar_background);
         schoolNameInfo = (LinearLayout) view.findViewById(R.id.school_name_info);
