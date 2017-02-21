9e9cce52b59f389d2d2d47647913b7f34055141c
achowdhury3762 <achowdhury3762@gmail.com>
2017-02-21 05:12:10 -0500

    Added database functionality, click to remove

--- app/src/main/java/nyc/c4q/ashiquechowdhury/schoolme/favoritesrecycler/SchoolViewHolder.java
+++ app/src/main/java/nyc/c4q/ashiquechowdhury/schoolme/favoritesrecycler/SchoolViewHolder.java
@@ -3,7 +3,9 @@ package nyc.c4q.ashiquechowdhury.schoolme.favoritesrecycler;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import it.sephiroth.android.library.picasso.Picasso;
 import nyc.c4q.ashiquechowdhury.schoolme.R;
 import nyc.c4q.ashiquechowdhury.schoolme.models.SchoolDbModel;
 
@@ -14,13 +16,16 @@ import nyc.c4q.ashiquechowdhury.schoolme.models.SchoolDbModel;
 public class SchoolViewHolder extends RecyclerView.ViewHolder {
     private ImageView schoolPictureImage;
     private ImageView closeButton;
+    private TextView schoolNameTextView;
 
     public SchoolViewHolder(View itemView) {
         super(itemView);
         schoolPictureImage = (ImageView) itemView.findViewById(R.id.school_image);
+        schoolNameTextView = (TextView) itemView.findViewById(R.id.school_TV);
     }
 
-    public void bind(SchoolDbModel schoolDbModel) {
-        schoolPictureImage.setImageResource(R.drawable.ic_home_black_24dp);
+    public void bind(final SchoolDbModel schoolDbModel) {
+        Picasso.with(itemView.getContext()).load(schoolDbModel.getPictureURL()).fit().into(schoolPictureImage);
+        schoolNameTextView.setText(schoolDbModel.getSchoolName());
     }
 }
