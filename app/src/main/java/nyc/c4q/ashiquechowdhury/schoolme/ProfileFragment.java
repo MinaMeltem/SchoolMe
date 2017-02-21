2a6362f06a3f873803aed2c8663febab54782435
achowdhury3762 <achowdhury3762@gmail.com>
2017-02-21 14:46:16 -0500

    Setup Profile Fragment

--- app/src/main/java/nyc/c4q/ashiquechowdhury/schoolme/ProfileFragment.java
+++ app/src/main/java/nyc/c4q/ashiquechowdhury/schoolme/ProfileFragment.java
@@ -5,20 +5,26 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 import nyc.c4q.ashiquechowdhury.schoolme.models.SchoolsResponse;
 
 public class ProfileFragment extends Fragment {
     private String TAG = SchoolsResponse.class.getSimpleName();
+    ImageView gifImage;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, parent, false);
+        gifImage = (CircleImageView) view.findViewById(R.id.profile_image);
         return view;
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
-
+        Glide.with(getContext()).load(R.raw.calvin).into(gifImage);
     }
 }
